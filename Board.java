8a32ddba5abd8272237c2ad867919064100fa198
Ryan Harang <ryh110.rh@gmail.com>
2023-04-27 13:02:43 -0700

    Skeletons

---
+++ Board.java
@@ -0,0 +1,12 @@
+class Board {
+    private rooms = Room[];
+
+    public Board() {
+        //set rooms
+        //set adjacencies?
+    }
+    
+    public void getRooms() {
+        return this.rooms;
+    }
+}
---
+++ CastingOffice.java
@@ -0,0 +1,13 @@
+class CastingOffice {
+
+    private info = int[5][3];
+
+    public CastingOffice() {
+        
+    }
+
+    public int[][] getInfo() {
+        return this.info;
+    }
+
+}
