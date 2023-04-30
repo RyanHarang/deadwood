6e72ba139239d148465e0f9bbcbabffce7d85266
Ryan <harangr@wwu.edu>
2023-04-30 13:09:17 -0700

    added little to casting CastingOffice

--- CastingOffice.java
+++ CastingOffice.java
@@ -1,16 +1,25 @@
 public class CastingOffice {
 
-    private int[][] info;
+    private static int[][] info = {
+            { 2, 4, 5 },
+            { 3, 10, 10 },
+            { 4, 18, 15 },
+            { 5, 28, 20 },
+            { 6, 40, 25 }
+    };
 
     public CastingOffice() {
-        
+
     }
 
-    public int[][] getInfo() {
-        return this.info;
+    public static int[][] getInfo() {
+        return info;
     }
-    
+
     public boolean validate(Player player) {
+        // asks location manager to ensure player is in Casting Office
+        // ensures player has valid credit or money amounts to declare
+        // the upgrade option they choose
         return false;
     }
 
