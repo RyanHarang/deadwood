1faa4f417c182830498a6c539c32a0e11a8e21e8
Ryan <harangr@wwu.edu>
2023-05-04 10:50:48 -0700

    Made InputParser

--- InputParser.java
+++ InputParser.java
@@ -4,9 +4,5 @@ public class InputParser {
 
     }
 
-    // method for passing input in a useable form
-    // to the correct class, input comes from InputManager
-    public void passInput(String input) {
-
-    }
 }
+
