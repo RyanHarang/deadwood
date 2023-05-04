df6495e11be6d7e2b22faa42b7fda3491033fbe1
Ryan <harangr@wwu.edu>
2023-05-04 10:48:38 -0700

    Made InputParser to satisfy MVC

---
+++ InputParser.java
@@ -0,0 +1,12 @@
+public class InputParser {
+
+    public InputParser() {
+
+    }
+
+    // method for passing input in a useable form
+    // to the correct class, input comes from InputManager
+    public void passInput(String input) {
+
+    }
+}
