268d7f7c3e24376e77a993aa93ad472d67259bdf
Alexandre <labbea@wwu.edu>
2023-05-11 13:41:38 -0700

    player actions and deadwood

---
+++ Parser.java
@@ -0,0 +1,13 @@
+public class Parser {
+
+    //constructor
+    public Parser(){}
+
+    //methods
+    public void parse(){
+
+    }
+    private void apply(){
+        
+    }
+}
---
+++ PlayerActions.java
@@ -0,0 +1,104 @@
+import java.util.ArrayList;
+public class PlayerActions {
+    private Dice dice = new Dice();
+    public void playerMove(Player p, LocationManager locationManager, Board board, InpParser inpP, CastingOffice castingOffice, CurrencyManager currencyManager){
+        // prompt for new location
+        Room playerLocation = locationManager.getPlayerLocation(p);
+        Room location = board.roomByName(inpP.getDestination(playerLocation.neighborsString()));
+        // in theory, the way getDestination is implemented in InpManager should force a
+        // valid room name to be returned
+
+        locationManager.move(p, location);
+        if(!location.getScene().isFaceUp()){
+            location.getScene().flip();
+        }
+        // must then check: scene card face up or down?
+        // would you like to take a roll?
+        if(inpP.takingRole()){
+            playerTakeRole();
+        }
+        // open roles on scene or room?
+        // does player want to take a role?
+        if(location.getName().equals("office"));
+        if(inpP.upgrade()){
+            playerUpgrade(p, inpP, castingOffice, locationManager, currencyManager);
+        }
+        // if invalid, repeat. if valid, prompt upgrade.
+    }
+
+    public void playerTakeRole(InpParser inpP, Player p, Room location){
+        //list possible roles
+        //get role the user wants
+        //give player that role
+        String roles = "";
+        ArrayList<Role> offCardList = location.getRoles();
+        ArrayList<Role> onCardList = location.getScene().getRoles();
+        //get roles available to take
+        ArrayList<Role> availableRoles = new ArrayList<Role>();
+        for(Role r: offCardList){
+            if(r.isOccupied()){
+                availableRoles.add(r);
+            }
+        }
+        for(Role r: onCardList){
+            if(r.isOccupied()){
+                availableRoles.add(r);
+            }
+        }
+        Role role = inpP.takeRole(availableRoles);
+        role.setOccupant(p);
+
+    }
+
+    public void playerAct(Player p, LocationManager locationManager, CurrencyManager currencyManager){
+        Room room = locationManager.getPlayerLocation(p);
+        if (p.getRole().isMain()) {
+            if (actOnCard(p, room.getScene().getBudget(), currencyManager)) {
+                room.removeShot();
+            }
+        } else {
+            actOffCard(p, room.getScene().getBudget(), currencyManager);
+        }
+        if (room.getShots() == 0) {
+            currencyManager.wrapPay(room);
+        }
+    }
+
+    private boolean actOnCard(Player player, int roomBudget, CurrencyManager currencyManager) {
+        int roll = dice.roll(player.getPracticeChips());
+        // success
+        if (roll >= roomBudget) {
+            currencyManager.adjustCredits(2, player);
+            return true;
+        }
+        // falure - prompt failure with model
+        return false;
+    }
+
+    private void actOffCard(Player player, int roomBudget, CurrencyManager currencyManager) {
+        int roll = dice.roll(player.getPracticeChips());
+        // success
+        if (roll >= roomBudget) {
+            currencyManager.adjustCredits(1, player);
+            currencyManager.adjustMoney(1, player);
+        }
+        // falure - prompt failure with model
+        else {
+            currencyManager.adjustMoney(1, player);
+        }
+    }
+
+    public void playerUpgrade(Player p, InpParser inpP, CastingOffice castingOffice, LocationManager locationManager, CurrencyManager currencyManager){
+        int[] upgrade = inpP.upgradeInfo();
+        int rank = upgrade[1];
+        Boolean upgradingWithMoney = false;
+        if (upgrade[0] == 1) {
+            upgradingWithMoney = true;
+        }
+        castingOffice.upgrade(p, rank, upgradingWithMoney, locationManager, currencyManager);
+    }
+
+    public void playerRehearse(Player p){
+        p.addPracticeChip();
+    }
+}
