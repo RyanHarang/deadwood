39a63ae14594f61a7d1931ccbb67b3fc7353771d
Ryan <120701017+RyanHarang@users.noreply.github.com>
2023-05-22 10:13:04 -0700

    added functiona;ity for deactivating buttons

--- Control.java
+++ Control.java
@@ -8,20 +8,36 @@ public class Control {
 
     }
 
-    public void move() {
+    public String move(String id) {
+        if (id.equals("move")) {
+            return "deactivatedMove";
+        }
         System.out.println("move clicked");
+        return "move";
     }
 
-    public void upgrade() {
+    public String upgrade(String id) {
+        if (id.equals("upgrade")) {
+            return "deactivatedUpgrade";
+        }
         System.out.println("upgrade clicked");
+        return "upgrade";
     }
 
-    public void act() {
+    public String act(String id) {
+        if (id.equals("act")) {
+            return "deactivatedAct";
+        }
         System.out.println("act clicked");
+        return "act";
     }
 
-    public void rehearse() {
+    public String rehearse(String id) {
+        if (id.equals("rehearse")) {
+            return "deactivatedRehearse";
+        }
         System.out.println("rehearse clicked");
+        return "rehearse";
     }
 
     public void endTurn() {
