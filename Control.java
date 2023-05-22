35046ebb1f1491264de574361c42c6be4da34c36
Ryan <120701017+RyanHarang@users.noreply.github.com>
2023-05-21 20:33:19 -0700

    Made control to interact with view

---
+++ Control.java
@@ -0,0 +1,30 @@
+public class Control {
+
+    public Control() {
+
+    }
+
+    public void startGame() {
+
+    }
+
+    public void move() {
+        System.out.println("move clicked");
+    }
+
+    public void upgrade() {
+        System.out.println("upgrade clicked");
+    }
+
+    public void act() {
+        System.out.println("act clicked");
+    }
+
+    public void rehearse() {
+        System.out.println("rehearse clicked");
+    }
+
+    public void endTurn() {
+        System.out.println("endTurn clicked");
+    }
+}
